3be9346e93156a78ae8328eac0697590cc71abe8
Graham De Young <grahamd0#gmail.com>
2026-09-06 15:10:57 -0500

    Added movement, controller input

--- player/player.gd
+++ player/player.gd
@@ -2,17 +2,21 @@ extends CharacterBody3D
 
 @export var mouse_h_sensitivity: float = 0.02
 @export var mouse_v_sensitivity: float = 0.02
+@export var joypad_h_sensitivity: float = 0.06
+@export var joypad_v_sensitivity: float = 0.03
 @export var camera_tilt_min: float = -60.0
 @export var camera_tilt_max: float = 30.0
 @export var default_fov: float = 75.0
 @export var fov_max: float = 80.0
 @export var spring_arm_default_length: float = 5.0
 @export var spring_arm_max_length: float = 8.0
+@export var invert_v_look: bool = true
 
 @onready var pivot_horizontal: Node3D = %PivotHorizontal
 @onready var pivot_vertical: Node3D = %PivotVertical
 @onready var camera: Camera3D = %Camera3D
 @onready var spring_arm: SpringArm3D = $CameraMount/PivotHorizontal/PivotVertical/SpringArm3D
+@onready var avatar: Node3D = $Avatar
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
@@ -24,26 +28,17 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	pass
-	## Add the gravity.
-	#if not is_on_floor():
-		#velocity += get_gravity() * delta
-#
-	## Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-#
-	## Get the input direction and handle the movement/deceleration.
-	## As good practice, you should replace UI actions with custom gameplay actions.
-	#var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	#var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	#if direction:
-		#velocity.x = direction.x * SPEED
-		#velocity.z = direction.z * SPEED
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
-		#velocity.z = move_toward(velocity.z, 0, SPEED)
-#
-	#move_and_slide()
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
+	_handle_controller_camera_movement()
+	_handle_ground_movement()
+	
 
 
 func _input(event: InputEvent) -> void:
@@ -71,7 +66,7 @@ func _input(event: InputEvent) -> void:
 			else:
 				spring_arm.spring_length = spring_arm_default_length
 		pivot_horizontal.rotation.y += -event.relative.x * mouse_h_sensitivity
-		
+
 	
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed('ui_cancel'):
@@ -79,3 +74,41 @@ func _unhandled_input(event: InputEvent) -> void:
 			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 		else:
 			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+			
+			
+func _handle_controller_camera_movement() -> void:
+	# camera vertical (controller):
+	var camera_v := -Input.get_axis("camera_forward", "camera_backward")
+	if invert_v_look:
+		camera_v = -camera_v
+	if !is_zero_approx(camera_v):
+		print("camera_v: %f" % camera_v)
+		var cam_min_angle_rads := deg_to_rad(camera_tilt_min)
+		var cam_max_angle_rads := deg_to_rad(camera_tilt_max)
+		pivot_vertical.rotation.x += camera_v * joypad_v_sensitivity
+		pivot_vertical.rotation.x = clampf(
+			pivot_vertical.rotation.x,
+			cam_min_angle_rads,
+			cam_max_angle_rads
+			)
+	# camera horizontal (controller):
+	var camera_h := Input.get_axis("camera_left", "camera_right")
+	if !is_zero_approx(camera_h):
+		pivot_horizontal.rotation.y += -camera_h * joypad_h_sensitivity
+	
+
+func _handle_ground_movement() -> void:
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	direction = direction.rotated(Vector3.UP, pivot_horizontal.rotation.y)
+	if direction:
+		avatar.look_at(global_position - direction, Vector3.UP, true)
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
